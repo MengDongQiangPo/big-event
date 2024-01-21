62700f87d0a636cf9c7bd0b18b7101479e40a90d
强迫萌动 <73006502+MengDongQiangPo@users.noreply.github.com>
2024-01-21 15:47:55 +0800

    Update AliOssUtil.java

--- src/main/java/com/research/utils/AliOssUtil.java
+++ src/main/java/com/research/utils/AliOssUtil.java
@@ -16,8 +16,8 @@ public class AliOssUtil {
     private static final String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
     // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
     //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-    private static final String ACCESS_KEY_ID="LTAI5tBHHYUVovJhPYs3L9dT";
-    private static final String ACCESS_KEY_SECRET="ConVokgsoERVe9B7RGxdD7aN6KJmVr";
+    private static final String ACCESS_KEY_ID="";
+    private static final String ACCESS_KEY_SECRET="";
     // 填写Bucket名称，例如examplebucket。
     private static final String BUCKET_NAME = "sau-big-event";
 
